d77f3b360f6b17a390838f116a751e8add5d1602
Phuc Le Dien <phucledien@gmail.com>
2018-05-20 13:52:44 +0700

    Updated index.js | Added spam change group avatar feature

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     page.setViewport({ width: 1280, height: 720 });
     await page.goto('https://www.messenger.com/', { waitUntil: 'networkidle2' });
@@ -12,6 +12,7 @@ const puppeteer = require('puppeteer');
 
     await emailField.type('YourFuckingEmail@SomeFuckingCompany');
     await passwordField.type('YourFuckingPassword');
+    
     
     // Wait til submit button is clicked and the page is loaded
     const navigationPromise = page.waitForNavigation();
@@ -26,11 +27,31 @@ const puppeteer = require('puppeteer');
     const MESSENGER_URL = 'https://www.messenger.com/t/199901607012936/';
 
     await page.goto(MESSENGER_URL, { waitUntil: 'networkidle2' });     
-    
-    // Your fucking Spam Message
-    setInterval( async () => {
-        await page.type('._1p1t', 'RIP FITZ!!! :(');
-        await page.keyboard.press('Enter');
+
+    /*********************************************************************
+     *                  SPAM CHANGE GROUP AVATAR                         *
+     *********************************************************************/
+    const pictures = ['./sticker.png', './sticker2.jpg', './sticker3.png']    
+    setInterval(async () => {
+        const elementsHandle = await page.$('input[type=file]');
+        for (let picture of pictures) {
+            await elementsHandle.uploadFile(picture);
+        }
     }, 1000);
+
+
+    /*********************************************************************
+     *                          SPAM MESSAGE                             *
+     *********************************************************************/
+    // Your fucking Spam Message
+    // setInterval( async () => {
+    //     page.type('._1p1t', 'Chó Phương :poop:')
+    //         .then(() => {
+    //             page.keyboard.press('Enter');
+    //         })
+    //         .catch((e) => {
+    //             console.log("Just to quickly!!! But it's still running!");
+    //         });
+    // }, 1000);
 })();
 
