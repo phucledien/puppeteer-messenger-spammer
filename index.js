886cceba532d8ec2ca0185f37a6d36865c56b1b1
Phuc Le Dien <phucledien@gmail.com>
2018-05-20 14:03:46 +0700

    Updated index.js | Change to headless browser

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     page.setViewport({ width: 1280, height: 720 });
     await page.goto('https://www.messenger.com/', { waitUntil: 'networkidle2' });
